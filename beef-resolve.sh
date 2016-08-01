f100d85a5b9f85bc91a04798aeb4189f3ea9bf18
ziflar <zembla2009@gmail.com>
2016-08-01 06:12:15 +0100

    Update beef-resolve.sh

--- beef-resolve.sh
+++ beef-resolve.sh
@@ -9,12 +9,16 @@
 # - Run:
 # ./beef_resolv.sh - Installs all dependencies. Best run from Kali Linux
 
+BOLD="\033[01;01m"     # Highlight
+RED="\033[01;31m"      # Issues/Errors
+GREEN="\033[01;32m"    # Success
+YELLOW="\033[01;33m"   # Warnings/Information
+RESET="\033[00m"       # Normal
 clear
-interfa=$(route | grep default | awk '{print $8}')
-ip=$(ifconfig $interfa | awk '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print substr($2,1,length($2))}')
+ip=$(ip route show | awk '(NR == 2) {print $9}')
 service beef-xss stop
 if [ -d "/usr/share/beef-xss" ]; then
-echo && echo -e " \e[1;31mbeef-xss exist \e[0m";
+echo && echo -e ${BOLD}'\nbeef-xss exist \n'${RESET}
 fi
 sleep 1
 rm -r /usr/share/beef-xss
@@ -29,9 +33,9 @@ sleep 2
 cd beef-xss
 sleep 2
 echo && echo 
-echo -e "\e[1;32m[*] Locate At Your config.yaml\e[0m \e[1;31mdb_file: "beef.db"\e[0m \e[1;32mand change to\e[0m \e[1;31mdb_file: "db/beef.db"\e[0m\e[1;32m,save and exit to contunie\e[0m"
+echo -e ${YELLOW}'\n\n[*] Locate At Your config.yaml'${RESET}${BOLD}' db_file: "beef.db"'${RESET} ${YELLOW}',change to'${RESET} ${BOLD}'db_file: "db/beef.db"'${RESET}${YELLOW}',save and exit to contunie'${RESET}
 echo && echo
-echo -e " \e[1;32m[*] Wait To Open config.yaml......\e[0m"
+echo -e ${YELLOW}'[*] Wait To Open config.yaml......'${RESET}
 sleep 4
 echo
 leafpad config.yaml
@@ -45,35 +49,31 @@ sleep 2
 clear
 port=$(ruby -ryaml -e "print YAML.load_file('/etc/beef-xss/config.yaml')['beef']['http']['port']")
 gnome-terminal -x sh -c 'cd /usr/share/beef-xss && ./beef; exec bash'
-echo -e " \e[1;32m[*] Wait To Be Redirected To Your BeEF Web Interface......\e[0m"
+echo -e ${BOLD}'[*] Wait To Be Redirected To Your BeEF Web Interface......'${RESET}
 sleep 2
-echo -e " \e[1;32m[*] When it's done, Login To Your BeEF Control Panel With (user/pass)=beef,To Generate a Beef Database\e[0m"
+echo -e ${YELLOW}"[*] When it's done, Login To Your BeEF Control Panel With (user/pass)=beef,To Generate a Beef Database"${RESET}
 sleep 15
 firefox http://$ip:$port/ui/panel
 sleep 6
-echo && echo
-echo -e " \e[1;32m Have You Been loged To Your BeEF Control Panel ? (Y/N)\e[0m"
+echo && echo -e ${RED}'\n\n Have You Been loged To Your BeEF Control Panel ? (Y/N)\n\n'${RESET}
 read install
 if [[ $install = Y || $install = y ]] ; then
-echo -e "\e[1;32m[*] Wait Please\e[0m"
-sleep 2
-echo && echo
-echo "[*] Wait Beef Is Stopping Service Now ."
+echo -e ${BOLD}'[*] Wait Please Beef Is Stopping Service Now......'${RESET}
 killall ruby && service beef-xss stop
 rm -r /usr/share/beef-xss/db/beef.db
 service beef-xss start
 sleep 2
 service beef-xss force-reload
 sleep 8
-echo "[*] It's Done Your beef-xss Is Fixed"
-echo && echo -e "Do An Update To Your beef-xss ? (Y/N)"
+echo && echo -e ${GREEN}"[*] It's Done Your beef-xss Is Fixed"${RESET}
+echo && echo -e ${YELLOW}"Do An Update To Your beef-xss ? (Y/N)"${RESET}
 read update
 if [[ $update = Y || $update = y ]] ; then
 cd /usr/share/beef-xss && ./update-beef
 else
-echo -e "[-] You Can Do It After run: cd /usr/share/beef-xss && ./update-beef"
+echo -e ${GREEN}"[-] You Can Do It After run: cd /usr/share/beef-xss && ./update-beef"${RESET}
 fi
 else
 killall ruby
-echo -e "\e[1;32m[-] there is an error run the script again !\e[0m"
+echo -e ${RED}"[-] there is an error run the script again !"${RESET}
 fi
